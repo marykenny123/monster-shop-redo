f1fae53831a36d841fe18a7a4865c3724908cfab
mary kenny <marykenny2704@yahoo.com>
2025-06-23 18:47:42 +0200

    initial commit

---
+++ src/main/java/com/redofromscratch/monster_shop_redo/MonsterShopRedoApplication.java
@@ -0,0 +1,13 @@
+package com.redofromscratch.monster_shop_redo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonsterShopRedoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonsterShopRedoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redofromscratch/monster_shop_redo/MonsterShopRedoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redofromscratch.monster_shop_redo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonsterShopRedoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
